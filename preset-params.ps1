926320332edcf4d05d06e697141c851f9330fff0
Alexey Evdokimov <a.evdokimov@russianit.ru>
2020-05-07 19:55:57 +0400

    change preset params setting

--- preset-params.ps1
+++ preset-params.ps1
@@ -1,14 +1,42 @@
 param(
     [Parameter(Position = 1, Mandatory = $true)]
-    [string]$preset = 'Z'
+    [string]$preset = 'Z',
+    [Parameter(Position = 2, Mandatory = $true)]
+    [string]$iniLocation = './settings',
+    [Parameter(Mandatory = $true)]
+    [string]$tcBuild,
+    [Parameter(Mandatory = $true)]
+    [string]$tcAddress,
+    [Parameter(Mandatory = $true)]
+    [string]$tcUser,
+    [Parameter(Mandatory = $true)]
+    [string]$tcPwd
 )
 
 . ./common/functions.ps1
 
-if ($preset -match '^(S|M|L|XL)$') {
-    $presetProperties = IniProperties "./presets/$preset.ini"
+$auth = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $tcUser, $tcPwd)))
 
-    $presetProperties.Keys | % {
-        "##teamcity[setParameter name='$_' value='$($presetProperties.Item($_))']"
-    }
+$presetParams = @{}
+if ($preset -match '^(S|M|L|XL)$') {
+    $presetParams = IniProperties "./presets/$preset.ini"
+}
+
+$buildParams = (Invoke-RestMethod `
+        -Headers @{Authorization = ("Basic {0}" -f $auth) } `
+        -Method Get `
+        -UseBasicParsing `
+        -ContentType 'application/json' `
+        -Uri "$tcAddress/httpAuth/app/rest/builds/id:$tcBuild/resulting-properties").properties.property | Where-Object { $_.name -like '*.ini.*' }
+
+$buildParams | ForEach-Object { $presetParams[$_.name] = $_.value }
+
+$inis = Get-ChildItem $iniLocation -Name '*.ini'
+
+foreach ($ini in $inis) {
+    $iniContent = IniProperties "$iniLocation/$ini"
+
+    $presetParams | Where-Object { $_.name -like "$ini.*" } | ForEach-Object { $_.name -match "$ini\.(?<param>.+)" ; $iniContent[$Matches['param']] = $_.value }
+
+    $iniContent.Keys | ForEach-Object { "$_=$($iniContent[$_])" } > "$iniLocation/$ini"
 }
